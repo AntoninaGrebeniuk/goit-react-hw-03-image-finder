3b21a729c3bd0a91a99ad2addda17854b47a9104
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-06-03 00:11:25 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -42,46 +42,6 @@ export const Searchbar = ({ onSubmit }) => {
   );
 };
 
-// export class Searchbar extends Component {
-//   state = {
-//     query: '',
-//   };
-
-//   handleChange = e => {
-//     const { value } = e.target;
-
-//     this.setState({ query: value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-
-//     console.log(this.state);
-//   };
-
-//   render() {
-//     return (
-//       <header class="searchbar">
-//         <form class="form" onSubmit={this.handleSubmit}>
-//           <button type="submit" class="button">
-//             <span class="button-label">Search</span>
-//           </button>
-
-//           <input
-//             class="input"
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//             onChange={this.handleChange}
-//             value={this.state.value}
-//           />
-//         </form>
-//       </header>
-//     );
-//   }
-// }
-
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
